1f3feb88dc1ed2ad21fc1c272a6269c36ab03a12
Prashanth-Bhat-99 <prashanthbhatraja99@gmail.com>
2021-07-30 01:24:59 +0530

    2

---
+++ app/src/main/java/com/thunder/apps/securenumismatics/activities/ui/fragments/CurrencyFragment.kt
@@ -0,0 +1,21 @@
+package com.thunder.apps.securenumismatics.activities.ui.fragments
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.thunder.apps.securenumismatics.R
+import android.widget.Button as Button1
+
+class CurrencyFragment : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        findViewById<Button1>(R.id.navigation_orders).setOnClickListener {
+            val openURL = Intent(Intent.ACTION_VIEW)
+            openURL.data = Uri.parse("http://calc-currency.herokuapp.com")
+            startActivity(openURL)
+        }
+    }
+}
